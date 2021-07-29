5822079ec814c7a364eaa74b164f62761259eb11
Micaela Pedrazas <67972197+mpedrazah@users.noreply.github.com>
2021-07-29 11:23:02 -0500

    test

--- StiffDiagram.py
+++ StiffDiagram.py
@@ -25,16 +25,16 @@ obs=pd.read_csv("TWDB_MPGCD_WQs_Clipped.csv")
 
 
 #%%water levels
-wl_wells=pd.read_csv("TWDB_MPGCD_WLs_Clipped.csv")
+# wl_wells=pd.read_csv("TWDB_MPGCD_WLs_Clipped.csv")
 
-twdb_wl=pd.read_csv('WaterLevelsByCounty.csv',
-                    parse_dates=['Date'], dayfirst=False)
-twdb_wl['MeasurementDate'] = pd.to_datetime(twdb_wl.Date, dayfirst=False)
+# twdb_wl=pd.read_csv('WaterLevelsByCounty.csv',
+#                     parse_dates=['Date'], dayfirst=False)
+# twdb_wl['MeasurementDate'] = pd.to_datetime(twdb_wl.Date, dayfirst=False)
 
-twdb_wl.StateWellNumber=twdb_wl.StateWellNumber.astype(dtype='int32')
+# twdb_wl.StateWellNumber=twdb_wl.StateWellNumber.astype(dtype='int32')
 
-wl=pd.read_excel('FOIA.xls',sheet_name='WaterLevel')
-wl.MeasurementDate=pd.to_datetime(wl.MeasurementDate)
+# wl=pd.read_excel('FOIA.xls',sheet_name='WaterLevel')
+# wl.MeasurementDate=pd.to_datetime(wl.MeasurementDate)
 
 #%%
 
